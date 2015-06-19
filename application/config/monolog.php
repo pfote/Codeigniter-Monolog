1b123ac4e52a9210a27dd0e5f74dc6554c565cb3
rich <r.brooks@freerateupdate.com>
2015-06-18 21:27:22 -0400

    Adding support for Stackify and Loggly

--- application/config/monolog.php
+++ application/config/monolog.php
@@ -35,3 +35,11 @@ $config['gelf_port'] = '12201';
 
 /* Raven options */
 $config['raven_endpoint'] = 'http://api:key@localhost/1';
+
+/* Stackify options */
+$config['stackify_api_key'] = '';
+$config['stackify_application_name'] = '';
+$config['stackify_environment'] = '';
+
+/* Loggly options */
+$config['loggly_consumer_token'] = '';
